1c8f4420bd3428ce75545aa2060849c31f3c7a50
Nick Gauthier <ngauthier@gmail.com>
2013-08-18 14:37:52 -0400

    structure loading

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Refuelly::Application.config.secret_key_base = '70e2f1bc3683ca42512f2443390515564cfb9ce28bd412130bfc12d534087b02e27a6594ba83b2c703f1e0faa9711b42e40a5cd461f3b90d7e403d700893c8d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Refuelly::Application.config.session_store :cookie_store, key: '_refuelly_session'
